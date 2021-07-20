c144db6820384c03e33c78abc4a448c9e6ad206b
Dilip.Kaushalye <kaushalyedilip>
2021-07-20 08:25:42 +0530

    Initial commit

---
+++ src/main/java/com/techstack/config/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.techstack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /**
+         * Default configurations which will secure all the requests
+         */
+
+        /*
+         * http .authorizeRequests() .anyRequest().authenticated() .and()
+         * .formLogin().and() .httpBasic();
+         */
+
+        /**
+         * Custom configurations as per our requirement
+         */
+
+        http.authorizeRequests().antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated().antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll().and().formLogin().and().httpBasic();
+
+        /**
+         * Configuration to deny all the requests
+         */
+
+        /*
+         * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
+         * .httpBasic();
+         */
+
+        /**
+         * Configuration to permit all the requests
+         */
+
+        /*http.authorizeRequests().anyRequest().permitAll().and().formLogin().and()
+                .httpBasic();*/
+    }
+
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
+                .withUser("user").password("12345").authorities("read").and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }*/
+
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails userDetails1 = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails userDetails2 = User.withUsername("user").password("12345").authorities("read").build();
+
+        userDetailsService.createUser(userDetails1);
+        userDetailsService.createUser(userDetails2);
+        auth.userDetailsService(userDetailsService);
+    }*/
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/techstack/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.techstack.repository;
+
+import com.techstack.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	
+	List<Customer> findByEmail(String email);
+
+}
